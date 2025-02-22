9c3c3c557949a82bbdc3d7e6f78ee2430c74c50f
Subodh <skhanal5@terpmail.umd.edu>
2025-02-22 17:52:26 -0500

    Add unit test for activity

---
+++ activities_test.go
@@ -0,0 +1,49 @@
+package iplocate_test
+
+import (
+    "io"
+    "net/http"
+    "strings"
+    "testing"
+
+    "temporal-tryout/iplocate"
+
+    "github.com/stretchr/testify/assert"
+    "go.temporal.io/sdk/testsuite"
+)
+
+type MockHTTPClient struct {
+    Response *http.Response
+    Err      error
+}
+
+func (m *MockHTTPClient) Get(url string) (*http.Response, error) {
+    return m.Response, m.Err
+}
+
+func TestGetIP(t *testing.T) {
+    testSuite := &testsuite.WorkflowTestSuite{}
+    env := testSuite.NewTestActivityEnvironment()
+
+    mockResponse := &http.Response{
+        StatusCode: 200,
+        Body:       io.NopCloser(strings.NewReader("127.0.0.1\n")),
+    }
+
+    ipActivities := &iplocate.IPActivities{
+        HTTPClient: &MockHTTPClient{Response: mockResponse},
+    }
+    env.RegisterActivity(ipActivities)
+
+    val, err := env.ExecuteActivity(ipActivities.GetIP)
+    if err != nil {
+        t.Fatalf("Expected no error, got %v", err)
+    }
+
+    var ip string
+    val.Get(&ip)
+
+
+    expectedIP := "127.0.0.1"
+    assert.Equal(t, ip, expectedIP)
+}
