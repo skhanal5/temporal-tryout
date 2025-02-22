65e3b30d373895a98ce97da54bece74d69b996e2
Subodh <skhanal5@terpmail.umd.edu>
2025-02-22 17:53:55 -0500

    Add unit test for GetLocationInfo Activity

--- activities_test.go
+++ activities_test.go
@@ -47,3 +47,35 @@ func TestGetIP(t *testing.T) {
     expectedIP := "127.0.0.1"
     assert.Equal(t, ip, expectedIP)
 }
+
+func TestGetLocationInfo(t *testing.T) {
+    testSuite := &testsuite.WorkflowTestSuite{}
+    env := testSuite.NewTestActivityEnvironment()
+
+    mockResponse := &http.Response{
+        StatusCode: 200,
+        Body: io.NopCloser(strings.NewReader(`{
+            "city": "San Francisco",
+            "regionName": "California",
+            "country": "United States"
+        }`)),
+    }
+
+    ipActivities := &iplocate.IPActivities{
+        HTTPClient: &MockHTTPClient{Response: mockResponse},
+    }
+
+    env.RegisterActivity(ipActivities)
+
+    ip := "127.0.0.1"
+    val, err := env.ExecuteActivity(ipActivities.GetLocationInfo, ip)
+    if err != nil {
+        t.Fatalf("Expected no error, got %v", err)
+    }
+
+    var location string
+    val.Get(&location)
+
+    expectedLocation := "San Francisco, California, United States"
+    assert.Equal(t, location, expectedLocation)
+}
